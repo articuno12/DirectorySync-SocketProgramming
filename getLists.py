ab4062c14bad8640bcbec89586f101757b1ac7e8
articuno12 <kritikagr12@gmail.com>
2017-03-19 16:03:15 +0530

    Added function to Get Name,size,timestamp and type of each file in the directory having timestamp between the given starttimestamp and endtimestamp .

--- getLists.py
+++ getLists.py
@@ -2,7 +2,7 @@ import os #to execute needed os commands
 import re  # for checking regular expression
 
 # Get Name,size,timestamp and type of each file in the current directory
-def GetLongList() :
+def FindLongList() :
     LongList = []
     for filename in os.listdir('.') :
 
@@ -16,3 +16,21 @@ def GetLongList() :
         LongList.append(fileinfo)
 
     return LongList
+
+# Get Name,size,timestamp and type of each file in the directory
+# having timestamp between starttimestamp and endtimestamp
+def FindShortList(StartTimeStamp,EndTimeStamp) :
+    # Utility Varaiables
+    Name = 0
+    Mtime = 1
+    Size = 2
+
+    ShortList = []
+    # first acquire info about all the files and then filter
+    LongList = FindLongList()
+
+    for fileinfo in Longlist :
+        if fileinfo[Mtime] >= StartTimeStamp and fileinfo[Mtime] <= EndTimeStamp :
+            ShortList.append(fileinfo)
+
+    return ShortList
