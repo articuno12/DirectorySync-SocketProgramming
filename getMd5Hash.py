cff6684ae2992cb008e34d8d47be11e6e2092a6f
articuno12 <kritikagr12@gmail.com>
2017-03-19 15:21:15 +0530

    Added Check statements to check for file , raise exceptions and refactored the code

--- getMd5Hash.py
+++ getMd5Hash.py
@@ -2,25 +2,34 @@ import os #to execute needed os commands
 import hashlib # to find MD5HashSum
 
 def FindHash(path) :
+    # defining utility varaiables
+    FileChunkSize = 4096 # the ammout of data to read in one go from the file
 
-    MD5HashSum = hashlib.md5()
+    # Check if the path is a valid path that leades to a file or directory
+    if not os.path.exists(path) :
+        raise Exception('The given path does not exist. Cannot calculate MD5 - Hash \n The path passed is : ' + path)
 
-    try :
-        # Try opening the file
-        f = open(path, 'rb')
+    # Check if the given path is a file
+    if not os.path.isfile(path) :
 
-    except:
-        # You can't open the file for some reason
-        f.close()
-        raise Exception('Unable to read a file , Cannot genrate MD5HashSum : ' + path)
+        HashValue = hashlib.md5()
 
-    # Read file in chunks and update the hashsum
-    data = f.read(4096)
-    while(data) :
-        MD5HashSum.update(hashlib.md5(data).hexdigest())
-        data = f.read(4096)
+        try :
+            # Try opening the file
+            f = open(path, 'rb')
 
-    # Close the file
-    f.close()
+        except:
+            # Unable to  open the file
+            f.close()
+            raise Exception('The requested file cannot be opened. Cannot calculate MD5 - Hash \n The path passed is : ' + path)
 
-    return MD5HashSum.hexdigest()
+            # Read file in chunks and update the hashsum
+            data = f.read(FileChunkSize)
+            while(data) :
+                HashValue.update(hashlib.md5(data).hexdigest())
+                data = f.read(FileChunkSize)
+
+                # Close the file
+                f.close()
+
+                return HashValue.hexdigest()
