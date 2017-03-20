c2c019e983328e1dccecea2ff50802847141cb76
articuno12 <kritikagr12@gmail.com>
2017-03-20 18:13:34 +0530

    Global Values put in a seprate file and udp socket init added to server

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
+import select
+import socket
 import Utils.getLists as getLists
 import Utils.getMd5Hash as Md5Hash
 import Utils.getVerification as getVerification
+import Utils.globalValues as globalValues
 
-port = 60000
-host = ""
 
-port = 60000
-s = socket.socket()
-host = ""
+# SET UP TCP
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # to force the OS to free the port
 # if the port was in use earlier it will be taken and assigned to the current program
 # use with care
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host, port))
-s.listen(5)
+tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-filename = raw_input("Enter file to share:")
-print 'Server listening....'
+tcp.bind((globalValues.host, globalValues.port))
+tcp.listen(2)
+
+# SET UP UDP
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind((globalValues.host,globalValues.port))
+
+logfile = open('log_server.txt','w')
 
 while True:
     conn, addr = s.accept()
