ea50599e899b8ab47779a350eb85d5b9d11f49ec
articuno12 <kritikagr12@gmail.com>
2017-03-20 18:37:48 +0530

    Added Recieve function part to first recieve the length of the incoming word

--- Utils/tcp.py
+++ Utils/tcp.py
@@ -1,6 +1,7 @@
 import pickle # for serialization
 import struct # to store numbers into string of fixed length
 import globalValues
+import select
 
 # To send data of at max size 10 MB
 def SendWord(conn,data) :
@@ -20,3 +21,20 @@ def SendWord(conn,data) :
     # Now send the actual data
     conn.sendall(data)
     return True
+
+
+# To recieve data of at max fize 10 MB
+def RecieveWord(conn) :
+    # First recieve the total length of the incoming data
+    conn.setblocking(0)
+    data = ''
+    toberecieved = 4
+    while toberecieved > 0 :
+        ready = select.select([conn],[],[],1)
+        if ready[0] :
+            recieved = conn.recv(toberecieved)
+            data = data + recieved
+            toberecieved -= len(recieved)
+    data, = struct.unpack('!I', data)
+
+    toberecieved = data
