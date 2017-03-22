b090b574c816d4bd54cc00fc599e09c51b257637
articuno12 <kritikagr12@gmail.com>
2017-03-22 19:06:46 +0530

    Added command handling download via tcp client

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import select
 import socket
 import sys
+import os
 import Utils.getLists as getLists
 import Utils.getMd5Hash as Md5Hash
 import Utils.getVerification as getVerification
@@ -34,11 +35,61 @@ def Execute(Command) :
             for fileinfo in result :
                 print fileinfo[filename] , fileinfo[timestamp] ,fileinfo[size]
 
-        
+        elif Command[0] == 'download' :
+            if Command[1] == 'tcp' :
+                # if the recieved arguments are None => requested file was a file
+                # else it was a directory
+                tobedownloaded = tcpWord.Recieve(conn)
+
+                # if the request was a directory
+                if tobedownloaded :
+                    # find the directories that have no file in them
+                    directories = []
+                    for fileinfo in tobedownloaded :
+                        if fileinfo[1] == 'directory' :
+                            directories.append(fileinfo[0])
+
+                    # make the required directories
+                    for directory in directories :
+                        if not os.path.exists(directory) :
+                            os.makedirs(directory)
+
+                    for fileinfo in tobedownloaded :
+                        if fileinfo[1] == 'file' :
+                            path = fileinfo[0]
+                            path = path.split('/')[:-1]
+                            path = '/'.join(path)
+
+                            if not os.path.exists(path) :
+                                os.makedirs(path)
+
+
+                    # download each file in the filelist
+                    # first close the current port
+
+                    tcp.close()
+                    sock.close()
+                    print >> logfile , "TCP port closed"
+                    print >> logfile , "UDP port closed"
+                    print >> logfile , "Command Executed successfully , directory structure recieved"
+
+                    for fileinfo in tobedownloaded :
+
+                        filepath = fileinfo[0]
+                        # remove './' from path
+                        filepath = filepath.split('/')
+                        filepath = filepath[1:]
+                        filepath = '/'.join(filepath)
+
+                        # Make the download command
+                        newcommand = 'download tcp ' + filepath
+                        Execute(newcommand)
+
+            
 while True :
     try  :
         Command =  raw_input("input : ")
-        # UserCommand = 'index longlist'
+        # Command = 'index longlist'
         print >> logfile , "User Command is ",Command
         ConnectToListner(Command)
 
